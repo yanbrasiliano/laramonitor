becf05ae09ffde822590c96285cbcfc7d623061e
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2024-02-04 19:49:02 -0300

    dev: Add Create Site.

--- resources/views/admin/sites/list.blade.php
+++ resources/views/admin/sites/list.blade.php
@@ -13,7 +13,23 @@
 <body>
     <div class="container mx-auto my-8 max-w-4xl">
         <div class="flex justify-between items-center mb-4">
-            <h2 class="text-2xl font-bold text-center">Sites List</h2>
+            <a href="{{ route('dashboard') }}">
+                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6">
+                    <path
+                        d="M22 2H2C0.9 2 0 2.9 0 4V16C0 17.1 0.9 18 2 18H9V20H7C6.45 20 6 20.45 6 21C6 21.55 6.45 22 7 22H17C17.55 22 18 21.55 18 21C18 20.45 17.55 20 17 20H15V18H22C23.1 18 24 17.1 24 16V4C24 2.9 23.1 2 22 2ZM22 16H2V4H22V16Z"
+                        fill="#4A90E2" />
+                    <circle cx="12" cy="14" r="1" fill="#4A90E2" />
+                    <path
+                        d="M16 12H8C7.45 12 7 11.55 7 11C7 10.45 7.45 10 8 10H16C16.55 10 17 10.45 17 11C17 11.55 16.55 12 16 12Z"
+                        fill="#4A90E2" />
+                    <path
+                        d="M14 8H10C9.45 8 9 7.55 9 7C9 6.45 9.45 6 10 6H14C14.55 6 15 6.45 15 7C15 7.55 14.55 8 14 8Z"
+                        fill="#4A90E2" />
+                </svg>
+
+            </a>
+
+
             <button onclick="openModal()"
                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create</button>
         </div>

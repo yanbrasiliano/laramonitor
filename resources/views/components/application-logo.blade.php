0ab4707d5ad3488a9634bf8d2f391fb0d2160ae5
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2024-02-04 15:09:11 -0300

    dev: Add Laravel Breeze, Auth, Login/Logout and new Welcome page.

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,17 @@
+<svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+        class="h-16 w-auto bg-gray-100 dark:bg-gray-900">
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+            class="h-16 w-auto bg-gray-100 dark:bg-gray-900">
+            <path
+                d="M22 2H2C0.9 2 0 2.9 0 4V16C0 17.1 0.9 18 2 18H9V20H7C6.45 20 6 20.45 6 21C6 21.55 6.45 22 7 22H17C17.55 22 18 21.55 18 21C18 20.45 17.55 20 17 20H15V18H22C23.1 18 24 17.1 24 16V4C24 2.9 23.1 2 22 2ZM22 16H2V4H22V16Z"
+                fill="#4A90E2" />
+            <circle cx="12" cy="14" r="1" fill="#4A90E2" />
+            <path
+                d="M16 12H8C7.45 12 7 11.55 7 11C7 10.45 7.45 10 8 10H16C16.55 10 17 10.45 17 11C17 11.55 16.55 12 16 12Z"
+                fill="#4A90E2" />
+            <path d="M14 8H10C9.45 8 9 7.55 9 7C9 6.45 9.45 6 10 6H14C14.55 6 15 6.45 15 7C15 7.55 14.55 8 14 8Z"
+                fill="#4A90E2" />
+        </svg>
+    </svg>
+</svg>

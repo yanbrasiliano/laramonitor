5c5bd6367fba28f5b2d328eac4f540559335aca1
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2024-02-14 20:30:11 -0300

    feat: adjust in blades sites and endpoints.

--- resources/views/admin/endpoints/index.blade.php
+++ resources/views/admin/endpoints/index.blade.php
@@ -61,7 +61,7 @@
 
             action.innerHTML =
                 `<div class="inline-flex">
-                 <svg xmlns="http://www.w3.org/2000/svg" class="edit-icon h-6 w-6 text-blue-500 hover:text-blue-700 cursor-pointer mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" title="Editar" data-id="${endpoint.id}" data-url="${endpoint.endpoint}">
+                 <svg xmlns="http://www.w3.org/2000/svg" class="edit-icon h-6 w-6 text-blue-500 hover:text-blue-700 cursor-pointer mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" title="Editar" data-id="${endpoint.id}" data-url="${endpoint.url}">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 4.232a1.5 1.5 0 00-2.121 0l-6.899 6.899a1 1 0 00-.263.464l-1.414 5.657a1 1 0 001.263 1.263l5.657-1.414a1 1 0 00.464-.263l6.899-6.899a1.5 1.5 0 000-2.121l-2.121-2.121z"/>
                     <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 15.5H8v.5h-.5v-.5zm4-4H12v.5h-.5v-.5z"/>
                     </svg>
